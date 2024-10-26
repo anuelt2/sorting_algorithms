ff5558db08a45aefc568f31c05af8670cf1f5daa
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-26 21:41:56 +0000

    Add 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,57 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence
+ *
+ * @array: The array to be sorted
+ * @size: The number of elements in @array
+ */
+
+void shell_sort(int *array, size_t size)
+{
+	size_t interval;
+	ssize_t i;
+	size_t j;
+
+	interval = 1;
+	while (interval < size / 3)
+	{
+		interval = interval * 3 + 1;
+	}
+
+	for (interval = interval; interval >= 1; interval = (interval - 1) / 3)
+	{
+		for (j = interval; j < size; j++)
+		{
+			for (i = j - interval; i >= 0; i -= interval)
+			{
+				if (array[i + interval] > array[i])
+				{
+					break;
+				}
+				else
+				{
+					swap(&array[i + interval], &array[i]);
+				}
+			}
+		}
+		print_array(array, size);
+	}
+}
+
+/**
+ * swap - Swaps two values
+ *
+ * @a: Pointer to first value to swap
+ * @b: Pointer to second value to swap
+ */
+
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
