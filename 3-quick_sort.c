6c5f72359da9e2a23483e7bf1ddbd1d94c37597e
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-26 15:11:22 +0000

    Modify 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * quick_sort - Sorts an array of itegers in ascending order
+ * quick_sort - Sorts an array of integers in ascending order
  * using the Quick sort algorithm
  *
  * @array: The array to be sorted
