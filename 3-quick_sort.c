56ce5e2a67309f86ef82b42d6f4988e9aaf8dee8
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-26 14:56:39 +0000

    Add 3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Sorts an array of itegers in ascending order
+ * using the Quick sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: The size of @array
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	int pivot;
+	size_t i;
+	size_t j;
+	static int *o_array;
+	static size_t o_size;
+
+	if (o_array == NULL)
+	{
+		o_array = array;
+		o_size = size;
+	}
+	if (size < 2)
+		return;
+	pivot = array[size - 1];
+	i = 0;
+	for (j = 0; j < size - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(o_array, o_size);
+			}
+			i++;
+		}
+	}
+	if (i != size - 1)
+	{
+		swap(&array[i], &array[size - 1]);
+		print_array(o_array, o_size);
+	}
+	quick_sort(array, i);
+	quick_sort(array + i + 1, size - i - 1);
+
+	if (array == o_array && size == o_size)
+	{
+		o_array = NULL;
+		o_size = 0;
+	}
+}
+
+/**
+ * swap - Swaps two values
+ *
+ * @a: Pointer to first value to swap
+ * @b: Pointer to second value to swap
+ */
+
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
