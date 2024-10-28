35e7aa63b06d339e288977bbc643f735762fb967
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2024-10-28 19:27:38 +0000

    Add sort_function function prototype

--- sort.h
+++ sort.h
@@ -25,6 +25,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void sort_function(int *array, size_t size, int *o_array, size_t o_size);
 void swap(int *a, int *b);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
